6254ea0c83d75bbaba4184418b259eb22df496aa
root <root@DESKTOP-790HRKS.localdomain>
2024-02-02 13:03:20 -0800

    Lowest common ancestor

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -7,8 +7,7 @@
  *
  * Return: If no common ancestors return NULL, else return common ancestor.
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-		const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
 {
 	binary_tree_t *mom, *pop;
 
