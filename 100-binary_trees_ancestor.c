c8d5ca3c241773f2fd8b28b145d96ed857c78127
root <root@DESKTOP-790HRKS.localdomain>
2024-02-03 06:04:12 -0800

    Lowest common ancestor

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,25 +1,40 @@
 #include "binary_trees.h"
+#include "10-binary_tree_depth.c"
 
 /**
- * binary_trees_ancestor - Finds the lowest common ancestor of two nodes.
- * @first: Pointer to the first node.
- * @second: Pointer to the second node.
+ * binary_trees_ancestor - Finds the lowest common ancestor of two nodes
+ * @first: Pointer to the first node
+ * @second: Pointer to the second node
  *
- * Return: If no common ancestors return NULL, else return common ancestor.
+ * Return: Pointer to the lowest common ancestor node, or NULL if not found
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+										const binary_tree_t *second)
 {
-	binary_tree_t *mom, *pop;
+	int diff;
 
-	if (!first || !second)
-		return (NULL);
-	if (first == second)
-		return ((binary_tree_t *)first);
-
-	mom = first->parent, pop = second->parent;
-	if (first == pop || !mom || (!mom->parent && pop))
-		return (binary_trees_ancestor(first, pop));
-	else if (mom == second || !pop || (!pop->parent && mom))
-		return (binary_trees_ancestor(mom, second));
-	return (binary_trees_ancestor(mom, pop));
+	if (first && second)
+	{
+		diff = binary_tree_depth(first) - binary_tree_depth(second);
+		if (diff < 0)
+		{
+			diff *= -1;
+			while (diff--)
+				second = second->parent;
+		}
+		else
+		{
+			while (diff--)
+				first = first->parent;
+		}
+		while (first)
+		{
+			if (first == second)
+				return ((binary_tree_t *) first);
+			first = first->parent;
+			second = second->parent;
+		}
+	}
+	return (NULL);
 }
